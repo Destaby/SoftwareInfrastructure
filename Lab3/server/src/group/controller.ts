b94667397c80891a29af162c3891253be252330f
oleg <olleglion02@gmail.com>
2022-11-21 12:24:19 +0200

    Endpoints for students and groups were added

--- Lab3/server/src/group/controller.ts
+++ Lab3/server/src/group/controller.ts
@@ -1,11 +1,32 @@
 import * as express from 'express';
+import * as uuid from 'uuid';
 import { GroupService } from './service';
 
 const app = express();
 
 export default ({ groupService }: { groupService: GroupService }) => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  app.use(express.json());
+  
   app.get('/', async (_, res) => {
+    const groups = await groupService.table.select();
+    res.json(groups).end();
+  });
+  
+  app.post('/', async (req, res) => {
+    const group = req.body;
+    const sameGroups = await groupService.table.select(['*'], group);
+    if (!sameGroups.length) {
+      group.id = uuid.v4();
+      await groupService.table.insert(group);
+    }
+    const groups = await groupService.table.select();
+    res.json(groups).end();
+  });
+  
+  app.delete('/', async (req, res) => {
+    const group = req.body;
+    await groupService.table.delete(group);
     const groups = await groupService.table.select();
     res.json(groups).end();
   });
--- Lab3/server/src/student/controller.ts
+++ Lab3/server/src/student/controller.ts
@@ -1,11 +1,32 @@
 import * as express from 'express';
+import * as uuid from 'uuid';
 import { StudentService } from './service';
 
 const app = express();
 
 export default ({ studentService }: { studentService: StudentService }) => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  app.use(express.json());
+  
   app.get('/', async (_, res) => {
+    const students = await studentService.storage.find();
+    res.json(students).end();
+  });
+  
+  app.post('/', async (req, res) => {
+    const student = req.body;
+    const check = await studentService.storage.findOne(student);
+    if (!check) {
+      student.id = uuid.v4();
+      await studentService.storage.create(student);
+    }
+    const students = await studentService.storage.find();
+    res.json(students).end();
+  });
+  
+  app.delete('/', async (req, res) => {
+    const student = req.body;
+    await studentService.storage.deleteOne(student);
     const students = await studentService.storage.find();
     res.json(students).end();
   });
